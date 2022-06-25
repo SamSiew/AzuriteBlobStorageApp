d7242c1d654ab4a8ce563b08765fcb749cc9ec79
SamSiew <Sam.Siew@Nintex.com>
2022-06-26 01:46:48 +0800

    Add Interface and AzureBlobStorageClientV11

---
+++ AzureBlobStorageAppClient/AzureBlobStorageClientV11.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorageAppClient
+{
+    public class AzureBlobStorageClientV11 : IAzureBlobStorage
+    {
+        private CloudBlobContainer CloudBlobContainer { get; set; }
+        public AzureBlobStorageClientV11(CloudStorageAccount cloudStorageAccount, string containername)
+        {
+            this.CloudBlobContainer = cloudStorageAccount.CreateCloudBlobClient().GetContainerReference(containername);
+            this.CloudBlobContainer.CreateIfNotExistsAsync();
+        }
+
+        public async Task UploadTextFile(string filename, string data)
+        {
+            var blobReference = CloudBlobContainer.GetBlockBlobReference(filename);
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(data), false))
+            {
+                await blobReference.UploadFromStreamAsync(ms);
+            }
+        }
+
+        public async Task<string> ReadTextFile(string filename)
+        {
+            var blobReference = CloudBlobContainer.GetBlockBlobReference(filename);
+            var blobContentAsString = await blobReference.DownloadTextAsync();
+            return blobContentAsString;
+        }
+
+        public async Task DeleteTextFile(string filename)
+        {
+            var blobReference = CloudBlobContainer.GetBlockBlobReference(filename);
+            await blobReference.DeleteAsync();
+        }
+    }
+}
---
+++ AzureBlobStorageAppClient/IAzureBlobStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace AzureBlobStorageAppClient
+{
+    public interface IAzureBlobStorage
+    {
+        public Task UploadTextFile(string filename, string data);
+        public Task<string> ReadTextFile(string filename);
+        public Task DeleteTextFile(string filename);
+    }
+}
